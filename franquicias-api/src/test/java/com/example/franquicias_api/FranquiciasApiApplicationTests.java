15d227ec78eb11d560f0b872c21dbaf58d8995e6
David Leonardo Gómez Ochoa <81239203+oxgerrero@users.noreply.github.com>
2026-05-08 23:39:41 -0500

    inicializacion de spring boot base proyecto

---
+++ franquicias-api/src/test/java/com/example/franquicias_api/FranquiciasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.franquicias_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FranquiciasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
